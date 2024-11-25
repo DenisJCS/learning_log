e98e42a43ee4f710eda8447f2a3f886fef0e2815
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-11-24 22:17:06 -0500

    First commint

---
+++ learning_logs/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render,redirect 
+
+from .models import Topic
+from .forms import TopicForm, EntryForm
+# Create your views here.
+def index(request):
+    """Домашняя страница приложения learning_log"""
+    return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """Выводии список тем."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """Выводит одну тему и все её записи"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Определяет новую тему"""
+    if request.method != 'POST':
+        """Данные не отправились : создается пустая форма"""
+        form = TopicForm()
+    else:
+        """Отправленные данные POST: обработать данные"""
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+    #Вывести пустую или недействительную форму
+    context = {'form':form}
+    return render(request,'learning_logs/new_topic.html', context)
+    
+def new_entry(request, topic_id):
+    """Добавляет новую запись по конкретной теме."""
+    # Получаем тему по её ID
+    topic = Topic.objects.get(id=topic_id)
+    
+    if request.method != 'POST':
+        # Данные не отправлены; создаётся пустая форма
+        form = EntryForm()
+    else:
+        # Обрабатываем отправленные данные POST
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            # Создаём объект записи, но пока не сохраняем в базу
+            new_entry = form.save(commit=False)
+            # Привязываем запись к конкретной теме
+            new_entry.topic = topic
+            # Сохраняем запись в базе
+            new_entry.save()
+            # Перенаправляем пользователя на страницу темы
+            return redirect('learning_logs:topic', topic_id=topic_id)
+    
+    # Если форма пустая или недействительная, отобразить её снова
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
