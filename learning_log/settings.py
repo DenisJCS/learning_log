2f643d45c2de3bed4a4bb365965284a3f5e40ec6
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-12-15 21:22:13 -0500

    Update settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -125,4 +125,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #My settings
-LOGIN_REDIRECT_URL = 'learning_log:index'
+LOGIN_REDIRECT_URL = 'learning_logs:index'
