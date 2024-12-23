d5d9bcda476e250b2bbb773837628f922bf29257
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-12-23 12:21:55 -0500

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login 
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def register(request):
+    """Register new user"""
+    if request.method !='POST':
+        #Output empty form
+        form = UserCreationForm()
+    else:
+        # Proccesing filled form
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # Log in and redirecting to home page
+            login(request, new_user)
+            return redirect('learning_logs:index')
+    
+    #Show empty page
+    context = {'form':form}
+    return render(request, 'user/register.html', context)
